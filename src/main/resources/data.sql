b2dcb1c809d35918983308765b15174a43c385bb
hoon25 <cchoon95@hanmail.net>
2024-08-23 13:25:05 +0900

    Chore(sql): init data 설정 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into member (email, password, name, blocked)
+values ("test1@naver.com", "test12", "테스트1", 0),
+       ("test2@naver.com", "test12", "테스트2", 0);
--- src/main/resources/db/sql/schema.sql
+++
@@ -1,7 +0,0 @@
-create table member
-(
-    member_id bigint not null auto_increment,
-    name      varchar(255),
-    password  varchar(255),
-    primary key (member_id)
-) engine=InnoDB
